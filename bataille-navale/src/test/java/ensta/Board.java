62744efe8cc88bc2ca50125caac227f561fbb5f7
qingyao_wang <qingyao.wang@qq.com>
2021-02-26 20:03:22 +0100

    Exercice 1

--- bataille-navale/src/test/java/ensta/Board.java
+++ bataille-navale/src/test/java/ensta/Board.java
@@ -1,32 +1,41 @@
 package ensta;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 public class Board {
 	
-	public String nom;
+	public String name;
 	public char[][] navires;
 	public boolean[][] frappes;
 	
-	public Board(String nom, int taille) {
+	
+	/**
+	 * Board Constructor
+	 * @param name String
+	 * @param size int
+	 */
+	public Board(String name, int size) {
 		
-    	this.nom=nom;
-    	this.frappes=new boolean[taille][taille];
-    	this.navires=new char[taille][taille];
-        for (int i=0;i<taille;i++)
-        	for (int j=0;j<taille;j++)
+    	this.name=name;
+    	this.frappes=new boolean[size][size];
+    	this.navires=new char[size][size];
+        for (int i=0;i<size;i++)
+        	for (int j=0;j<size;j++)
         	{
         		this.frappes[i][j]=false;
             	this.navires[i][j]='.';
         	}
     }
 	
+	/**
+	 * Board Constructor with default size=10
+	 * @param name String
+	 */
 	public Board(String nom) {	
     	this(nom,10);
     }
 	
+	/**
+	 * Print Board
+	 */
 	void print() {
 		int taille=this.frappes.length;
 		String s="";
@@ -35,7 +44,7 @@ public class Board {
 		s="  ";
 		
 		for (int i=0;i<taille;i++) {
-			int ascii=65+i;
+			int ascii=65+i; //  ASCII of 'A' is 65
 			s+=" "+ (char)ascii ;
 		}
 		s+="       "+s;
